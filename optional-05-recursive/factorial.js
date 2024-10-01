71b542626f41a0f1886c3f1cafd3cdd1e8bb9297
romiardanaputra <romiardana21@gmail.com>
2024-10-02 00:24:47 +0800

    feat: finish task 5 recursive

--- optional-05-recursive/factorial.js
+++ optional-05-recursive/factorial.js
@@ -1,5 +1,8 @@
 function factorial(n) {
-
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 // Jangan hapus kode di bawah ini!
--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,15 @@
 function fibonacci(n) {
-
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
+  } else {
+    let sequence = fibonacci(n - 1);
+    sequence.push(
+      sequence[sequence.length - 1] + sequence[sequence.length - 2]
+    );
+    return sequence;
+  }
 }
 
 // Jangan hapus kode di bawah ini!
